e93761ca1373c9a00649479a2e966a2d9580c5f4
EmreJM <emre.olgun@myy.haaga-helia.fi>
2017-12-04 17:15:17 +0000

    Update startvariety.sh

--- startvariety.sh
+++ startvariety.sh
@@ -6,9 +6,14 @@ echo " - Changes keyboard layout to finnish "
 echo " - Installs updates "
 echo " - Installs git "
 echo " - Installs puppet "
+echo " - A Vagrantfile will be created under /vagrant "
 echo " - Copies vagrantpp module from https://github.com/EmreJM/vagrantpp "
 echo " - Moves module to where it should be which is /etc/puppet/modules "
-echo " - Applies the variety module "
+echo " - Applies the vagrantpp module "
+echo " - Configures the machines "
+echo " - slave01 is on centos-7.3 "
+echo " - slave02 is on fedora-26 "
+echo " - slave03 is on debian-8.1 "
 
 tput -T xterm sgr0
 
@@ -20,6 +25,8 @@ cd
 git clone https://github.com/EmreJM/vagrantpp.git
 cd vagrantpp && sudo cp -R  vagrantpp /etc/puppet/modules/
 sudo puppet apply -e 'class {"vagrantpp":}'
+cd vagrant
+vagrant up
 
 tput -T xterm setaf 6
 
